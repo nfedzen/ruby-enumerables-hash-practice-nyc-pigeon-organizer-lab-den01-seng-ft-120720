ee1aa9838921de1ad51789f6afdeed41f6639cff
Nathan Fedzen <nfedzen@hotmail.com>
2020-11-22 21:06:14 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,4 +1,7 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
-  data.each_with_object
+  data.each_with_object({}) do |(key,value), final_array|
+    value.each do |inner_key, values|
+      
+  end
 end
