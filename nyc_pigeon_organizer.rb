ff261ffc9effddb46055649e7e82de222b8276bd
Nathan Fedzen <nfedzen@hotmail.com>
2020-11-22 21:04:43 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,10 +1,4 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
-  new_data_set = {}
-  new_data_set = data.collect do |key, element|
-    element.collect do |name|
-      name
-    end
-  end
-  new_data_set
+  data.
 end
